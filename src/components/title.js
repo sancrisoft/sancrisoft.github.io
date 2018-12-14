32adfd6d72bd84295f1f8afa7f7b00bf06c6d19d
Alejandro M <chepitoco@gmail.com>
2018-12-14 18:16:01 -0500

    Adding initial changes about title component

---
+++ src/components/title.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import { H1, H2 } from './styledComponents'
+
+const Title = ({ type, text }) => {
+    switch(type) {
+        case 2 : return (<H2>{text}</H2>);
+        default: return (<H1>{text}</H1>);
+    }
+}
+
+Title.propTypes = {
+  type: PropTypes.number,
+}
+
+Title.defaultProps = {
+  type: 1,
+}
+
+export default Title
