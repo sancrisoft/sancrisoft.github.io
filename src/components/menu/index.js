08332dc26a6307e3f670f55f3de608dd84398ad3
Alejandro M <chepitoco@gmail.com>
2018-12-18 12:02:55 -0500

    Adding HOC component to validate if is mobile tablet o desktop view.

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,35 +1,36 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { MenuContainer } from './styledComponents'
-
-const Menu = (props) => {
-  console.log('props', props);  
-  return (
-    <MenuContainer>
-        <Link to="/" >
-            About Us
-        </Link>
-        <Link to="/" >
-            Case Studies
-        </Link>
-        <Link to="/" >
-            Careers
-        </Link>
-        <Link to="/" >
-            Get a Quote
-        </Link>
-        <Link to="/" >
-            EN | ES
-        </Link>
-    </MenuContainer>
-)}
-
+import Viewport from '../HOC/withViewportHandler'
+export class Menu extends PureComponent { 
+    render() {
+        return (
+            <MenuContainer>
+                <Link to="/" >
+                    About Us
+                </Link>
+                <Link to="/" >
+                    Case Studies
+                </Link>
+                <Link to="/" >
+                    Careers
+                </Link>
+                <Link to="/" >
+                    Get a Quote
+                </Link>
+                <Link to="/" >
+                    EN | ES
+                </Link>
+            </MenuContainer>
+        );
+    }
+}
 Menu.propTypes = {
+    viewport: PropTypes.object,
 }
 
 Menu.defaultProps = {
-    options: PropTypes.array,
 }
 
-export default Menu
+export default Viewport(Menu);
