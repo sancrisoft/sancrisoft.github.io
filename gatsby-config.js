6e87c03e342b03efe8c564d11651fe8959496ee2
Carlos Gonzalez <carlos@sancrisoft.com>
2018-12-27 10:46:25 -0400

    Cambiando colores de configuración a colores SS

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,8 +32,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#f28724',
+        theme_color: '#f28724',
         display: 'minimal-ui',
         icon: 'src/images/small-logo.png', // This path is relative to the root of the site.
       },
