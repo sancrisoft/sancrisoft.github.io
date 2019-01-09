2281c78e657525db09a8addd59fb7b8afc60eb0e
Miguel Bolivar <MBolivar360@gmail.com>
2019-01-09 18:40:26 -0400

    Recovering old configuration for original layout on Grid, and adjusting height of the grid

--- src/components/galleryGrid/styledComponents.js
+++ src/components/galleryGrid/styledComponents.js
@@ -3,8 +3,10 @@ import breakpoint from 'styled-components-breakpoint';
 
 export const GridContainer = styled.div`
   width: 100vw;
+  height: 50vh;
   display: grid;
   grid-template-columns: 1fr;
+  grid-auto-rows: minmax(100px, 25vh);
   ${breakpoint('tablet')`
     grid-template-columns: repeat(2, 1fr);
   `}
@@ -16,5 +18,12 @@ export const GridContainer = styled.div`
 export const ImageContainer = styled.div`
   grid-column: span ${({ colSpan }) => colSpan};
   grid-row: span ${({ rowSpan }) => rowSpan};
+  .gatsby-image-wrapper {
+    height: 100%;
+    width: 100%;
+    img {
+      object-fit: cover !important;
+    }
+  }
 `;
 
