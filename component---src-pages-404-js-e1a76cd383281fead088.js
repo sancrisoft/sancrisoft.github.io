d437c60512b33cb05a7702b27d4448ce5268aa9d
Alejandro M <chepitoco@gmail.com>
2019-05-27 18:42:29 -0500

    Updates

---
+++ component---src-pages-404-js-e1a76cd383281fead088.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{134:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(170),o=n(168);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-e1a76cd383281fead088.js.map
