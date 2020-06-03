8eae92923ef7fec29ba643871705b7d2c66f6d2e
Jhony Moncada <jhony@sancrisoft.com>
2020-06-03 08:10:15 -0500

    Cleaning up useless styles.

--- src/styles/targi/styledComponents.js
+++ src/styles/targi/styledComponents.js
@@ -229,140 +229,44 @@ export const BenefitCard = styled.div`
   `}
 `;
 
-
-
-
-
-export const ExperienceTitle = styled.h2`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 1.2em;
-  color: #000;
-  width: 100%;
+export const Footer  = styled.footer`
+  background-color: #1e1e26;
   text-align: center;
-`;
+  color: #888890;
+  a {
+    color: #f3f4f9;
+    text-decoration: none;
+  }
+  a:first-child {
+    margin-right: 1rem;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+  p {
+    margin: 1rem 0 0 0;
+  }
+  ${PageSizer} {
+    padding: 1rem;
 
-export const ExperienceSubtitle = styled.h3`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 2em;
-  padding-top: 14px;
-  color: #000;
-  width: 100%;
-  text-align: center;
   ${breakpoint('tablet')`
-    font-size: 2.5em;
+    display: flex;
+    justify-content: space-between;
+    flex-flow: row-reverse;
+    p {
+      margin: 0;
+    }  
   `}
   ${breakpoint('desktop')`
-    font-size: 3em;
   `}
+  }
 `;
 
 
-export const Experiences = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-wrap: wrap;
-  width: 100%;
-  padding-top: 4em;
-`;
 
-export const TargiSolveTitle = styled.h3`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 2em;
-  padding-top: 15px;
-  color: #000;
-  width: 100%;
-  ${breakpoint('tablet')`
-    font-size: 2.5em;
-  `}
-  ${breakpoint('desktop')`
-    font-size: 3em;
-  `}
-`;
 
-export const TargiSolveDesc = styled.p`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 1.2em;
-  padding-top: 20px;
-  color: #000;
-  font-family: Averta;
-  width: 100%;
-  text-align: center;
-`;
 
-export const BoxTargiSolve = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-wrap: wrap;
-  width: 100%;
-  padding-top: 4em;
-`;
 
-export const ContentSupportTeam = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  margin-top: 2em;
-  background: #ececec;
-  padding: 1.2em;
-`;
-
-export const SupportTeamText = styled.p`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 1.2em;
-  margin: 0;
-  margin-left: 1rem;
-  width: 80%;
-  ${breakpoint('tablet')`
-    width: 100%;
-  `}
-`;
-
-export const TitleBig = styled.h3`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 2em;
-  color: #000;
-  width: 100%;
-  padding-top: .5em;
-  text-align: center;
-  ${breakpoint('tablet')`
-    font-size: 2.5em;
-  `}
-  ${breakpoint('desktop')`
-    font-size: 3em;
-  `}
-`;
-
-export const TitleDesc = styled.p`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 1.2em;
-  padding-top: 1.2em;
-  color: #000;
-  font-family: Averta;
-  width: 80%;
-  margin: 0 auto;
-  text-align: center;
-  line-height: 1.4;
-  ${breakpoint('desktop')`
-    width: 750px;
-  `}
-`;
 
 export const SectionForm = styled.section`
   width: 100%;
@@ -541,49 +445,3 @@ export const WhatsappButton  = styled.a`
   }
 `;
 
-export const FooterInfo  = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  padding: 1em 2em;
-  align-items: center;
-  width: 100%;
-  max-width: 1200px;
-  margin: 0 auto;
-`;
-
-export const FooterLeft = styled.div`
-  display: flex;
-  align-item: center;
-  justify-content: flex-start;
-  width: 100%;
-  h2 {
-    font-size: 1em;
-    margin-bottom: 0;
-  }
-  ${breakpoint('tablet')`
-    width: 40%;
-  `}
-`;
-
-export const FooterRight = styled.div`
-  display: flex;
-  align-item: center;
-  width: 100%;
-  padding-top: 1em;
-  justify-content: space-between;
-  ${breakpoint('tablet')`
-    width: 60%;
-    padding-top: 0;
-    justify-content: flex-end;
-  `}
-`;
-
-export const FooterLink = styled.a`
-  display: flex;
-  font-size: .9em;
-  color: #f48600;
-  ${breakpoint('tablet')`
-    margin-left: 1.2em;
-  `}
-`;
