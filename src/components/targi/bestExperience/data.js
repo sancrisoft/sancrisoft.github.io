8cb063874a16fb91c7ebf96dd1c38f140e23a907
Bryan  <bryanvillamil@iMac-de-BrYaN.local>
2020-06-04 16:45:55 -0500

    images carousel page targi

--- src/components/targi/bestExperience/data.js
+++ src/components/targi/bestExperience/data.js
@@ -5,8 +5,17 @@ import Meat4 from '../../../images/targi/meat/meat-4.png'
 import Meat5 from '../../../images/targi/meat/meat-5.png'
 import Meat6 from '../../../images/targi/meat/meat-6.png'
 import Fish1 from '../../../images/targi/fish/fish-1.png'
+import Fish2 from '../../../images/targi/fish/fish-2.png'
+import Fish3 from '../../../images/targi/fish/fish-3.png'
+import Fish4 from '../../../images/targi/fish/fish-4.png'
+import Fish5 from '../../../images/targi/fish/fish-5.png'
+import Fish6 from '../../../images/targi/fish/fish-6.png'
 import Veg1 from '../../../images/targi/veg/veg-1.png'
-import Frame from '../../../images/targi/frame.png'
+import Veg2 from '../../../images/targi/veg/veg-2.png'
+import Veg3 from '../../../images/targi/veg/veg-3.png'
+import Veg4 from '../../../images/targi/veg/veg-4.png'
+import Veg5 from '../../../images/targi/veg/veg-5.png'
+import Veg6 from '../../../images/targi/veg/veg-6.png'
 
 export const dataCarousel = {
   meat: [
@@ -49,9 +58,29 @@ export const dataCarousel = {
     },
     {
       id: 2,
-      urlImage: Frame,
-      name: "Frame"
-    }
+      urlImage: Fish2,
+      name: "Fish 2"
+    },
+    {
+      id: 3,
+      urlImage: Fish3,
+      name: "Fish 3"
+    },
+    {
+      id: 4,
+      urlImage: Fish4,
+      name: "Fish 4"
+    },
+    {
+      id: 5,
+      urlImage: Fish5,
+      name: "Fish 5"
+    },
+    {
+      id: 6,
+      urlImage: Fish6,
+      name: "Fish 6"
+    },
   ],
   veg: [
     {
@@ -61,8 +90,28 @@ export const dataCarousel = {
     },
     {
       id: 2,
-      urlImage: Frame,
-      name: "Frame"
-    }
+      urlImage: Veg2,
+      name: "Veg 2"
+    },
+    {
+      id: 3,
+      urlImage: Veg3,
+      name: "Veg 3"
+    },
+    {
+      id: 4,
+      urlImage: Veg4,
+      name: "Veg 4"
+    },
+    {
+      id: 5,
+      urlImage: Veg5,
+      name: "Veg 5"
+    },
+    {
+      id: 6,
+      urlImage: Veg6,
+      name: "Veg 6"
+    },
   ],
 }
