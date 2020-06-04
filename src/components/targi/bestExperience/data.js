d8277471d750c25ef0decd4f30c6665c84450ae3
Bryan  <bryanvillamil@iMac-de-BrYaN.local>
2020-06-04 16:19:34 -0500

    add first set images in section carousel

---
+++ src/components/targi/bestExperience/data.js
@@ -0,0 +1,68 @@
+import Meat1 from '../../../images/targi/meat/meat-1.png'
+import Meat2 from '../../../images/targi/meat/meat-2.png'
+import Meat3 from '../../../images/targi/meat/meat-3.png'
+import Meat4 from '../../../images/targi/meat/meat-4.png'
+import Meat5 from '../../../images/targi/meat/meat-5.png'
+import Meat6 from '../../../images/targi/meat/meat-6.png'
+import Fish1 from '../../../images/targi/fish/fish-1.png'
+import Veg1 from '../../../images/targi/veg/veg-1.png'
+import Frame from '../../../images/targi/frame.png'
+
+export const dataCarousel = {
+  meat: [
+    {
+      id: 1,
+      urlImage: Meat1,
+      name: "Meat 1"
+    },
+    {
+      id: 2,
+      urlImage: Meat2,
+      name: "Meat 2"
+    },
+    {
+      id: 3,
+      urlImage: Meat3,
+      name: "Meat 3"
+    },
+    {
+      id: 4,
+      urlImage: Meat4,
+      name: "Meat 4"
+    },
+    {
+      id: 5,
+      urlImage: Meat5,
+      name: "Meat 5"
+    },
+    {
+      id: 6,
+      urlImage: Meat6,
+      name: "Meat 6"
+    },
+  ],
+  fish: [
+    {
+      id: 1,
+      urlImage: Fish1,
+      name: "Fish 1"
+    },
+    {
+      id: 2,
+      urlImage: Frame,
+      name: "Frame"
+    }
+  ],
+  veg: [
+    {
+      id: 1,
+      urlImage: Veg1,
+      name: "Veg 1"
+    },
+    {
+      id: 2,
+      urlImage: Frame,
+      name: "Frame"
+    }
+  ],
+}
